a5b81569048b6e07e43dbc597f31d66114546fdc
mplotnyko <mykhaylo.plotnykov@mail.utoronto.ca>
2026-05-06 16:50:51 -0400

    docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -45,7 +45,7 @@ html_title = "exopie"
 html_static_path = ["_static"] # Usually docs/source/_static/
 
 html_theme_options = {
-    "repository_url": "https://github.com/yourusername/exopie",
+    "repository_url": "https://github.com/mplotnyko/exopie",
     "use_repository_button": True,
     "use_issues_button": True,
     "use_edit_page_button": True,
